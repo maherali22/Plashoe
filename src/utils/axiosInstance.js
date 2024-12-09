ae75ede2ddfe36a49af56a513dcd7717d9f19ddc
Maher <mahiralimattummal@gmail.com>
2024-12-09 22:06:53 +0530

    update

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -7,5 +7,28 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use(
-  (config)
+  (config) => {
+    const accessToken = Cookies.get("token");
+    if (accessToken) {
+      config.headers["token"] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response)=> response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {originalRequest._retry = true;
+      try {
+        const response = await axios.get("URL_ADDRESS");
+        const newAccessToken = response.data.token;
+      }
+    }
+    return Promise.reject(error);
+  }
 )
